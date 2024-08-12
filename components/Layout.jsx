7f748adb4c9b7860e20bcf413de447f906b766c5
fukuda0801 <nagisano0801@gmail.com>
2024-08-12 15:03:30 +0900

    ssg: gray-matter追加 マークダウンファイルからデータ取得するため

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,23 +1,26 @@
 import Head from "next/head";
 import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../src/styles/utils.module.css";
 
-const name = "Shin Code";
+const name = "ハム太郎";
 export const siteTitle = "Next.js blog";
 
 function Layout({ children }) {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
+      <header className={styles.header}>
         <Image
           src="/images/profile.jpg"
           alt="プロフィール画像"
           width={100}
           height={100}
+          className={utilStyles.borderCircle}
         />
-        <h1>{name}</h1>
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
       </header>
       <main>{children}</main>
     </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,106 @@
 import { Inter } from "next/font/google";
 import Layout from "../../components/Layout";
+import utilStyle from "../styles/utils.module.css";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { getPostsData } from "../../lib/post";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+// SSGの場合
+
+export async function getStaticProps() {
+  const allPostsData = getPostsData(); // id title date, thumbnail
+  console.log(allPostsData);
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <>
-      <Layout>あいうえお</Layout>
+      <Layout>
+        <section className={utilStyle.headingMd}>
+          <p>
+            とっとこ走るよ。ハムスター軍団のリーダー。ロコちゃんのペット。なのだが口癖なのだ。
+          </p>
+        </section>
+
+        <section className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}>
+          <h2>📝エンジニアのブログ</h2>
+          <div className={styles.grid}>
+            <article>
+              <Link href="/">
+                <Image
+                  src="/images/thumbnail01.jpg"
+                  alt="画像"
+                  className={styles.thumbnailImage}
+                  width={100}
+                  height={100}
+                />
+              </Link>
+              <Link href="/" className={utilStyle.boldText}>
+                ハム太郎の歴史
+              </Link>
+              <br />
+              <small className={utilStyle.lightText}>February 23, 2020</small>
+            </article>
+            <article>
+              <Link href="/">
+                <Image
+                  src="/images/thumbnail01.jpg"
+                  alt="画像"
+                  className={styles.thumbnailImage}
+                  width={100}
+                  height={100}
+                />
+              </Link>
+              <Link href="/" className={utilStyle.boldText}>
+                ハム太郎の歴史
+              </Link>
+              <br />
+              <small className={utilStyle.lightText}>February 23, 2020</small>
+            </article>
+            <article>
+              <Link href="/">
+                <Image
+                  src="/images/thumbnail01.jpg"
+                  alt="画像"
+                  className={styles.thumbnailImage}
+                  width={100}
+                  height={100}
+                />
+              </Link>
+              <Link href="/" className={utilStyle.boldText}>
+                ハム太郎の歴史
+              </Link>
+              <br />
+              <small className={utilStyle.lightText}>February 23, 2020</small>
+            </article>
+            <article>
+              <Link href="/">
+                <Image
+                  src="/images/thumbnail01.jpg"
+                  alt="画像"
+                  className={styles.thumbnailImage}
+                  width={100}
+                  height={100}
+                />
+              </Link>
+              <Link href="/" className={utilStyle.boldText}>
+                ハム太郎の歴史
+              </Link>
+              <br />
+              <small className={utilStyle.lightText}>February 23, 2020</small>
+            </article>
+          </div>
+        </section>
+      </Layout>
     </>
   );
 }
